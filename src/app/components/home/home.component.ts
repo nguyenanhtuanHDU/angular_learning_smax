01245840fcb99814f39bb55337bb05b502717c25
nguyenanhtuanHDU <anhtuan02042002@gmail.com>
2023-11-18 08:41:08 +0700

    moment.js npm

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
+import { timer } from 'rxjs';
 import { IStudent } from 'src/app/models/student';
 import { IUser } from 'src/app/models/user';
 import { MainService } from 'src/app/services/main.service';
@@ -11,33 +13,19 @@ import { MainService } from 'src/app/services/main.service';
 export class HomeComponent implements OnInit {
   constructor(private mainService: MainService) {}
 
-  // user!: IUser;
   students: IStudent[] = [];
 
   ngOnInit(): void {
-    // this.getAllUser();
-    // this.getUserById(1);
-    this.getAllStudentByTeacherID();
-  }
-
-  // getAllUser() {
-  //   this.mainService.users.getAll().subscribe((data) => {
-  //     console.log('🚀 ~ data:', data);
-  //   });
-  // }
-
-  // getUserById(userID: number) {
-  //   this.mainService.users.getById(userID).subscribe((data) => {
-  //     this.user = data;
-  //   });
-  // }
-
-  getAllStudentByTeacherID() {
-    this.mainService.students.getAll('65295ec5119f52715f0b179b').subscribe(
-      (data) => {
-        this.students = data.data;
-      },
-      (err) => this.mainService.handleError(err)
-    );
+    const myTimer = timer(1000, 1000); // timer(time_delay, time_interval)
+    myTimer.subscribe(() => {
+      console.log('a');
+    });
+    const myTime = new Date();
+    let timeRemain = moment(myTime).diff(moment(), 'second'); // tính thời khoảng thời gian từ hiện tại đến myTime
+    console.log('🚀 ~ moment():', moment());
+    console.log('🚀 ~ timeRemain:', timeRemain);
+    console.log(moment().startOf('day').fromNow());
+    // ...
+    // url npm: https://momentjs.com/
   }
 }
